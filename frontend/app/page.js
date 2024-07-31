8f8ae0c7cd8b512f73bded7f51065a11d25ce665
nina031 <messaoudinina55@gmail.com>
2024-07-31 18:25:34 +0200

    add frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+'use client'
+import Helloworld from '@/components/Hello'
+
+function page() {
+  return (
+    <div><Helloworld/></div>
+  )
+}
+export default page
---
+++ frontend/components/Hello.jsx
@@ -0,0 +1,24 @@
+"use client"
+import React, { useEffect,useState }  from 'react'
+
+function Helloworld() {
+
+    const [message, setMessage] = useState("Loading");  
+  
+    useEffect(() => {
+      fetch("http://localhost:8080/api/home")
+      .then(
+          (response) => response.json()
+      )
+      .then(
+          (data) =>{
+              setMessage(data.message);
+          }
+      );
+    },[]);
+  
+    return <div>{message}</div>;
+    
+  }
+
+export default Helloworld;
